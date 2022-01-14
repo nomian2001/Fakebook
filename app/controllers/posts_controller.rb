a8c292c2b56018b7405635f07dfac8c561bfd4d7
nomian2001 <dttung19@apcs.vn>
2022-01-14 18:13:58 +0700

    Add friends part 1 (create model and click add)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
   def index
     @posts = current_user.posts.all.order("created_at DESC")
     @post = current_user.posts.build
+    @users = User.all
   end
 
   # GET /posts/1 or /posts/1.json
